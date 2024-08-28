0fc9cd66425f69c8cba582d2f0649b3374b8e1cf
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2024-08-28 12:45:24 -0400

    feat: file finalization and implementation

---
+++ JavaScript-Algorithms-and-Data-Structures/Build-a-Palindrome-Checker/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const checkButton = document.getElementById("check-btn");
+  let letters = document.querySelectorAll("h1 span");
+  const colors = ["#00915c", "#01adbd", "#0070de", "#2542e2", "#5526e3"];
+
+  const textInput = document.getElementById("text-input");
+  const result = document.getElementById("result");
+
+  for (let i = 0; i < letters.length; i += 2) {
+    let colorIndex = Math.floor(i / 2) % colors.length;
+    letters[i].style.color = colors[colorIndex];
+    if (i + 1 < letters.length) {
+      letters[i + 1].style.color = colors[colorIndex];
+    }
+  }
+
+  checkButton.addEventListener("click", function() {
+    if (textInput.value) {
+      let cleanInput = textInput.value.replace(/[^a-z0-9]/gi, '').toLowerCase();
+      let reversedInput = cleanInput.split("").reverse().join("");
+
+      console.log(cleanInput, reversedInput)
+      if (cleanInput === reversedInput) {
+        resultDisplay("is a palindrome");
+      } else {
+        resultDisplay("is not a palindrome");
+      }
+    } else {
+      alert("Please input a value");
+    }
+  })
+
+  function resultDisplay(resultText) {
+    document.getElementById("intro").classList.add("hide");
+    document.getElementById("result").classList.remove("hide");
+    document.getElementById("result").innerHTML =
+      `<strong>${textInput.value}</strong> ${resultText}`;
+  }
+});
