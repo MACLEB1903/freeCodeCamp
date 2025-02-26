e8f88c5bcf481340dfcddb91008c8fb7fee09c2c
MARCEL <115071244+marcelxeballar@users.noreply.github.com>
2025-02-26 15:03:07 -0400

    feat: add header component

--- Front-End-Development-Libraries/Build-a-Random-Quote-Machine/src/components/Header.tsx
+++ Front-End-Development-Libraries/Build-a-Random-Quote-Machine/src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
         <img src={quoteRight} className="h-[3.5rem] w-[3.5rem]" />
       </div>
       <a
-        className="text-white font-poppins ml-[1rem] font-semibold hidden md:block"
+        className="text-white font-poppins ml-[1rem] font-semibold hidden lg:block"
         target="_blank"
         href="https://github.com/macleb1903"
       >
